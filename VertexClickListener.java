32da5eab7ed5dbaf2824258a5812e0e0bbb99f10
pietro99 <43954467+pietro99@users.noreply.github.com>
2018-12-11 17:58:26 +0100

    VertexClickListener

--- VertexClickListener.java
+++ VertexClickListener.java
@@ -5,82 +5,31 @@ import java.util.HashMap;
 import java.awt.Color;
 import java.util.ArrayList;
 
-public class VertexClickListener extends MouseAdapter {
-	private Graph graph;
-	private static Color color = Color.BLACK;
-	private GraphComponent graphComponent;
-	final int DIAMETER = 16;
-	int counter=0;
-	private ArrayList<Integer> sequence = null;
-
-	public VertexClickListener (Graph graph, GraphComponent graphComponent) {
-		this.graph = graph;
-		this.graphComponent = graphComponent;
-		sequence = randomSequence(graph.getVertices().size());
-		graph.getVertices().get(sequence.get(0)).setBorderColor(Color.RED);
-	}
-
-	public void mouseClicked(MouseEvent e) {
-		if(!color.equals(Color.BLACK)) {//you can't colour a node in black you have to choose a colour
-			HashMap<Integer, Vertex> vertices = graph.getVertices();
-		    Vertex nextVertex = null;
-		    Vertex vertex = vertices.get(sequence.get(counter));
-			vertex.setBorderColor(Color.RED);
-
-			if(counter<(sequence.size()-1))
-				nextVertex = vertices.get(sequence.get(counter+1));
-
-		    if	(e.getX()>(vertex.getMidX()-DIAMETER/2) &&
-		    	e.getX()<(vertex.getMidX()+DIAMETER/2) &&
-		    	e.getY()>(vertex.getMidY()-DIAMETER/2) &&
-		    	e.getY()<(vertex.getMidY()+DIAMETER/2)
-		    										) {
-		    	vertex.setBorderColor(Color.BLACK);
-		    	if(nextVertex!=null)
-		    	nextVertex.setBorderColor(Color.RED);
-		    	counter++;
-		    	vertex.setColor(color);
-
-		    	if(!graph.check()) {//code in case of defeat
-		    		System.out.println("you lose");
-		    	}
-
-		    	if(counter==vertices.size()) {
-		    		counter=0;
-		    		if(graph.check()) {//code in case of victory
-		    			System.out.println("you win");
-		    		}
-		    	}
-		    	graphComponent.repaint();
-	            return;
-		    }
-		}
-	}
-
-	public static void setColor(Color col) {
-		  color = col;
-	  }
-
-	//this method generate a random sequence which is the order of colouring the nodes
-	private ArrayList<Integer> randomSequence(int a) {
-		ArrayList<Integer> sortedSequence = new ArrayList<Integer>();
-		ArrayList<Integer> randomSequence = new ArrayList<Integer>();
-
-		for(int i=0; i<=a; i++) {
-			sortedSequence.add(i);
-		}
-
-		for(int i=0; i<=a; i++) {
-			int index =(int)(Math.random()*sortedSequence.size());
-			randomSequence.add(sortedSequence.get(index));
-			sortedSequence.remove(index);
-		}
-		for(int i=0; i<=randomSequence.size(); i++) {
-			if(randomSequence.get(i)==0) {
-				randomSequence.remove(i);
-				break;
-			}
-		}
-		return randomSequence;
-	}
+//VertexClickListener checks whether the user clicked on a vertex, and updates the color of that vertex.
+class VertexClickListener extends MouseAdapter {
+private Graph graph;
+private GraphComponent graphComponent;
+private static Color color = Color.BLACK;
+final int DIAMETER = 16;
+public VertexClickListener (Graph graph, GraphComponent graphComponent) {
+ this.graph = graph;
+ this.graphComponent = graphComponent;
+}
+public void mouseClicked(MouseEvent e) {
+ HashMap<Integer, Vertex> vertices = graph.getVertices();
+ for (int i=1; i<=vertices.size(); i++) {
+   Vertex vertex = vertices.get(i);
+   if (e.getX()>(vertex.getMidX()-DIAMETER/2) &&
+       e.getX()<(vertex.getMidX()+DIAMETER/2) &&
+       e.getY()>(vertex.getMidY()-DIAMETER/2) &&
+       e.getY()<(vertex.getMidY()+DIAMETER/2)) {
+         vertex.setColor(color);
+         graphComponent.repaint();
+         return;
+       }
+ }
+}
+public static void setColor(Color col) {
+	  color = col;
+}
 }
