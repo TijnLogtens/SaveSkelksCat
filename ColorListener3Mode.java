6229a4311521de3e9742eab010cb0911fcb4daf6
pietro99 <43954467+pietro99@users.noreply.github.com>
2018-12-11 17:26:19 +0100

    ColorListener for the THIRD MODE

---
+++ ColorListener3Mode.java
@@ -0,0 +1,23 @@
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ColorListener3Mode implements MouseListener{
+		public void mouseClicked(MouseEvent e) {
+			Point p = new Point(e.getX(), e.getY());
+			for(int i=0; i<ColorGrid3Mode.getRect().length; i++) {
+			if(ColorGrid3Mode.getRect()[i].contains(p)) {
+				VertexClickListener3Mode.setColor(ColorGrid3Mode.getColor()[i]);
+			}
+			}
+			
+		}
+		public void mousePressed(MouseEvent e) {}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {
+			
+		}
+		public void mouseExited(MouseEvent e) {}
+
+	}
