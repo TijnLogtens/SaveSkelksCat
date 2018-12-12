b2f135c60895256d8bff76971358d9a9c78d56c6
eroskohen <43954648+eroskohen@users.noreply.github.com>
2018-12-12 15:20:22 +0100

    Create GraphGameResultScreen.java

---
+++ GraphGameResultScreen.java
@@ -0,0 +1,13 @@
+//Getters for the result screen
+
+	public double getTimer() {
+		return timer;
+	}
+		
+	public int getResult() {
+		return score;
+	}
+		
+	public String getGameMode() {
+		return gameMode;
+	}
